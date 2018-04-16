c31a70b7a36fe2d85700ea513d205aa4d4ba574a
Spencer Goodman <spencerwgoodman@icloud.com>
2018-04-16 01:02:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@ def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
 
-  elseif int % 5 == 0
+  else int % 5 == 0
     puts "Buzz"
 
-  elseif int % 15 == 0
+  else int % 15 == 0
     puts "FizzBuzz"
   end
 end
